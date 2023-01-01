761e164c4bca312309a311373f424f9b433e720c
DorianOfAstora <pisciotti.gianluca@gmail.com>
2023-01-02 00:15:29 +0100

    Primo commit del nuovo Document Converter e altre Utilities

---
+++ DaddaDocumentsConverter/DaddaDocumentsConverter/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DaddaDocumentsConverter;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DaddaDocumentsConverter/DaddaDocumentsConverter/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DaddaDocumentsConverter;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
